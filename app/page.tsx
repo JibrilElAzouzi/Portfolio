2c353d6d4d57492b5352ace80b143cc055daeb25
Jibril <stalax.pro@gmail.com>
2024-10-02 21:43:23 +0200

    Add my image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <section>
       <a href={socialLinks.linkedin} target="_blank">
         <Image
-          src="/profile.png"
+          src="/profile.jpg"
           alt="Profile photo"
           className="rounded-3xl bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
