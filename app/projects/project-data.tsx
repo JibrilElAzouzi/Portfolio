11cdb832cca5f748c3495575176cb64269d5aef6
Jibril <158104781+JibrilElAzouzi@users.noreply.github.com>
2024-10-23 15:01:09 +0200

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -6,25 +6,5 @@ export interface Project {
 }
 
 export const projects: Project[] = [
-  {
-    title: "Project One",
-    year: 2023,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Two",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
+
 ];
